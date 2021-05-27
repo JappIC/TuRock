e21b066b24f3f8d2cea332ec192742fc84f98b32
[Jonas] <[jonas.cn@gmail.com]>
2021-05-27 21:17:49 +0100

    Mi primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { GroupsComponent } from './components/pages/groups/groups.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'groups', component: GroupsComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    // Usa (routes), { useHash:true } si falla cuando usemos parámetros
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
